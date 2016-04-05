99449d4160146d5ba610599bc1401ee29db445c6
jonnaks <jonnaks@kth.se>
2016-04-05 15:53:27 +0200

    Html-pages och lite angular-pages

--- app/js/gameCtrl.js
+++ app/js/gameCtrl.js
@@ -1,9 +1,14 @@
 // Dinner controller that we use whenever we want to display detailed
 // information for one dish
-dinnerPlannerApp.controller('DishCtrl', function ($scope, $cookieStore, $routeParams,Dinner) {
+dinnerPlannerApp.controller('gameCtrl', function ($scope, $cookieStore, $routeParams,Dinner) {
   // TODO in Lab 5: you need to get the dish according to the routing parameter
   // $routingParams.paramName
   // Check the app.js to figure out what is the paramName in this case
+
+
+  $scope.quiz =function(){
+    alert("Valt sort du");
+  }
 
 	Dinner.Dish.get({id:$routeParams.dishId},function(data){
      	$scope.dish=data;
@@ -27,6 +32,7 @@ dinnerPlannerApp.controller('DishCtrl', function ($scope, $cookieStore, $routePa
   	}
 
     $scope.addToMenu = function(){
+      alert("valt geografi");
       var pendingDish = Dinner.getPendingDish();
       Dinner.addDishToMenu(pendingDish);
       $scope.pendingDishP = 0.00;
